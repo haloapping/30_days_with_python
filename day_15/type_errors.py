5f8fdf8d6d7292f08b25f318af4ff1c3afe27c29
Alfiyanto Kondolele <alfiyanto.kondolele@gmail.com>
2022-05-14 18:57:50 +0800

    Day 15

---
+++ day_15/type_errors.py
@@ -0,0 +1,32 @@
+# Syntax Error
+# print "halo"
+
+# Name Error
+# print(age)
+
+# Index Error
+# numbers = [1, 2, 3, 4, 5]
+# print(numbers[5])
+
+# Module Not Found Error
+# import maths
+
+# Attribute Error
+# import math
+# print(math.PI)
+
+# Key Error
+# user = {"firstname": "Alfiyanto", "lastname": "Kondolele", "age": 23}
+# print(user["gender"])
+
+# Type Error
+# print(4 + '4')
+
+# Import Error
+# from math import power
+
+# Value Error
+# print(int("12a"))
+
+# Zero Division Error
+# print(2 / 0)
